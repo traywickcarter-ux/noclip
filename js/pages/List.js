ff1e7895068b34d664807179c31c19527eddedb3
traywickcarter-ux <traywickcarter@gmail.com>
2026-07-15 02:36:09 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        levels like emptiness that use the X pos position bug cannot be accepted however extremely long levels are allowed
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        levels cannot be NSFW as we must have a showcase of them	
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        levels can be remakes of any level as long as its not an original level from the list
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Levels must have parts where the accuracy goes down below 85		
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        You must  use GDR for the viewing the accuracy abnd death count, but you can also use eclipse if on mobile or you prefer it
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        The levels must b only possible when using noclip
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Levels must have showcase
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        the password is the song not the password for the level
                     </p>
                 </div>
             </div>
